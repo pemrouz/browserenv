6affc1e445a756e931fb82171ee8be0b0c873965
pemrouz <p3pedram@hotmail.com>
2016-02-06 13:09:36 +0000

    only create window, document if on server

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-global.window = require('jsdom').jsdom('<div>').defaultView
-global.document = window.document
+var client = require('utilise/client')
+
+if (!client) {
+  global.window = require('jsdom').jsdom('<div>').defaultView
+  global.document = window.document
+}
