4384761bfea9822d10e1c69f9e65007d073a1ad4
pemrouz <p3pedram@hotmail.com>
2016-07-22 17:45:25 +0100

    feat: set return value to document

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var client = require('utilise/client')
-
+  
 if (!client) {
   global.window = require('jsdom').jsdom('<div>').defaultView
   global.document = window.document
+} else {
+  document = window.document
 }
+
+module.exports = document
